69999c4b066fa67d899687d593b4651a63d7cdf3
Captain-Fahd <omarfahd06@gmail.com>
2026-07-11 13:17:45 +1000

    Create Message entity with Spring JPA

---
+++ backend/src/main/java/io/github/cptfahd/backend/model/Message.java
@@ -0,0 +1,41 @@
+package io.github.cptfahd.backend.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "messages", indexes = {
+        @Index(name = "idx_messages_created_at", columnList = "created_at")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String text;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
